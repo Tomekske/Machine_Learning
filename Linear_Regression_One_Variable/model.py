012a20fc1c0d2486dae7d25a6517b7113a9d5ddc
Tomekske <joostenstomek@gmail.com>
2019-04-22 22:20:15 +0200

    log: 3
    - Minimise cost function
    - Split data
    - Transpose data

--- Linear_Regression_One_Variable/model.py
+++ Linear_Regression_One_Variable/model.py
@@ -3,6 +3,7 @@ import numpy as np
 import math
 import csv
 import time
+from sklearn import cross_validation
 
 def csvContent(filename):
     '''Get content of the csv file
@@ -51,18 +52,19 @@ def ProgramExecutionTime(start):
         currentTime = currentTime / 60
         print(f'Execution time: {currentTime:0.2f} min')
 
-def Hypothesis(X,W,b):
+def Hypothesis(W,B,X):
     '''Calculates the hypothesis using the lineair regression formula 'X * W + B'
     
     Args:
-        X (numpy array): Original x-values
         W (numpy array): Weight of the equation (slope)
         b (numpy array): Bias of the equation (offset)
+        X (numpy array): Original x-values
+
     Return:
         (Tensor): Y-value for fiven X-value
     '''
 
-    return tf.matmul(X, W) + b    
+    return tf.matmul(X, W) + B    
 
 def CostFunction(hx, Y):
     '''Calculates the cost function of the linear equation '1/n * sum((pred - y)^2)' 
@@ -75,6 +77,52 @@ def CostFunction(hx, Y):
     '''
 
     return  tf.reduce_mean(tf.square(hx - Y))
+
+def SplitData(featuresX, featuresY, testbatch):
+    '''Splits data into a training and test data set
+    
+    Args:
+        featuresX (numpy array): Array of X-features
+        featuresY (numpy array): Array of Y-features
+        testbatch (float): Test batch ratio rate
+    Return:
+        (numpy array): Returns XY-training and XY-test sets
+    '''
+
+    trainX, testX, trainY, testY = cross_validation.train_test_split(featuresX,featuresY, test_size=testbatch, random_state=42)
+
+    #Create a multidemensional array containing training data
+    training = np.array([trainX, trainY])
+    #Create a multidemensional array containing test data
+    test =  np.array([testX, testY])
+
+    return TransposeData(training, test)
+
+def TransposeData(trainingSet, testSet):
+    '''Transpose the data sets [data, features]
+    
+    Args:
+        trainingSet (numpy array): Array of training sets
+        testSet (numpy array): Array of test sets
+    Return:
+        (numpy array): Returns XY-training and XY-test sets
+    '''
+
+    #Split traing data into XY-arrays
+    trainX,trainY = trainingSet
+    #Split test data into XY-arrays
+    testX,testY = testSet
+
+    #Create multi demonsional array of the training data
+    trainX = np.array([trainX])
+    trainY = np.array([trainY])
+
+    #Create multi demonsional array of the test data
+    testX = np.array([testX])
+    testY = np.array([testY])
+
+    return np.transpose(trainX), np.transpose(testX), np.transpose(trainY), np.transpose(testY)
+
 #Program start time
 startTime = time.time()
 
@@ -82,8 +130,11 @@ startTime = time.time()
 csvList = csvContent('Linear_Regression_One_Variable/Dataset/simple.csv')
 
 #Get column data
-originalX = HeaderData(csvList, 'x')
-originalY = HeaderData(csvList, 'y')
+featuresX = HeaderData(csvList, 'x')
+featuresY = HeaderData(csvList, 'y')
+
+#Split data into training and test data (30% training data and 70% test data)
+trainX, testX, trainY, testY = SplitData(featuresX, featuresY, 0.3)
 
 #A placeholder is simply a variable that we will assign data to at a later date. 
 #It allows us to create our operations and build our computation graph, without needing the data.
@@ -95,9 +146,36 @@ W = tf.Variable(tf.zeros([1,1]), name = "weights")
 B = tf.Variable(tf.zeros([1]), name = "bias")
 
 #Calculate the hypothesis with random values
-pred = Hypothesis(X,W,B)
+pred = Hypothesis(W,B,X)
 #Calculate the cost function with the prediction of the random values
 cost = CostFunction(pred, Y)
+
+#minimise cost function parameters
+learningRate = 0.000000001
+epochs = 2000
+
+optimizer = tf.train.GradientDescentOptimizer(learningRate).minimize(cost)
+init = tf.initialize_all_variables()
+
+with tf.Session() as sesh:
+    sesh.run(init)
+
+    # loop-over all epochs
+    for epoch in range(epochs):
+        #Minimise cost function
+        sesh.run(optimizer, feed_dict= { X: trainX, Y: trainY})
+
+        #Calculate cost, weight and bias
+        c =  sesh.run(cost, feed_dict = {X: trainX, Y: trainY})
+        w = sesh.run(W)
+        b = sesh.run(B)
+        print(f'epoch: {epoch} c: {c:.6f} w: {w} b: {b}')
+
+    #Get weight and bias
+    weight = sesh.run(W)
+    bias = sesh.run(B)
+
+    print(f'Weight = {weight}\nBias = {bias}')
 
 #print program execution time
 ProgramExecutionTime(startTime)
