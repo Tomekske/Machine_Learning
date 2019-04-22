1fd0976d11d37c998843c6f6cab05cd2ac2c893e
Tomekske <joostenstomek@gmail.com>
2019-04-22 14:38:09 +0200

    log: 2
    - Calculate hypthesis
    - Calculate cost function

--- Linear_Regression_One_Variable/model.py
+++ Linear_Regression_One_Variable/model.py
@@ -26,7 +26,7 @@ def HeaderData(csvList,header):
         header (string): Header containing the associated data
 
     Return:
-        (numpy array) Returns a numpy array containing the data
+        (numpy array): Returns a numpy array containing the data
     '''
 
     data = []
@@ -51,6 +51,30 @@ def ProgramExecutionTime(start):
         currentTime = currentTime / 60
         print(f'Execution time: {currentTime:0.2f} min')
 
+def Hypothesis(X,W,b):
+    '''Calculates the hypothesis using the lineair regression formula 'X * W + B'
+    
+    Args:
+        X (numpy array): Original x-values
+        W (numpy array): Weight of the equation (slope)
+        b (numpy array): Bias of the equation (offset)
+    Return:
+        (Tensor): Y-value for fiven X-value
+    '''
+
+    return tf.matmul(X, W) + b    
+
+def CostFunction(hx, Y):
+    '''Calculates the cost function of the linear equation '1/n * sum((pred - y)^2)' 
+    
+    Args:
+        hx (numpy array): Predicted Y-values
+        Y (numpy array): Actual Y-values
+    Return:
+        (Tensor): Cost function value
+    '''
+
+    return  tf.reduce_mean(tf.square(hx - Y))
 #Program start time
 startTime = time.time()
 
@@ -60,6 +84,20 @@ csvList = csvContent('Linear_Regression_One_Variable/Dataset/simple.csv')
 #Get column data
 originalX = HeaderData(csvList, 'x')
 originalY = HeaderData(csvList, 'y')
+
+#A placeholder is simply a variable that we will assign data to at a later date. 
+#It allows us to create our operations and build our computation graph, without needing the data.
+X = tf.placeholder(tf.float32, [None, 1])
+Y = tf.placeholder(tf.float32,  [None, 1])
+
+#Create and initialize variables with zeros
+W = tf.Variable(tf.zeros([1,1]), name = "weights")
+B = tf.Variable(tf.zeros([1]), name = "bias")
+
+#Calculate the hypothesis with random values
+pred = Hypothesis(X,W,B)
+#Calculate the cost function with the prediction of the random values
+cost = CostFunction(pred, Y)
 
 #print program execution time
 ProgramExecutionTime(startTime)
